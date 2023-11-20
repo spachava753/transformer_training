eb1f699eb666bffd6654c6fc49e11f0d0ac67bb4
Shashank Pachava <spachava753@gmail.com>
2023-11-19 23:03:58 -0500

    Update configuration_compressed_llama.py

--- shared_mlp/compressed_llama/configuration_compressed_llama.py
+++ shared_mlp/compressed_llama/configuration_compressed_llama.py
@@ -1,28 +1,23 @@
 from transformers import LlamaConfig
-from typing import List, Union
+from typing import List, Union, Dict
 
 
 class CompressedLlamaConfig(LlamaConfig):
 
     def __init__(
         self,
-        share_layers: Union[List[List[int]], str] = "none",
+        tied_layers: Union[Dict[int, int], str] = "none",
         **kwargs,
     ):
-        if isinstance(share_layers, str) and share_layers not in ["none", "all"]:
-            raise ValueError(f"`share_layers` must be 'none' or all', got {share_layers}.")
-        if isinstance(share_layers, list):
-            already_shared = []
+        if isinstance(tied_layers, str) and tied_layers not in ["none", "all"]:
+            raise ValueError(f"`tied_layers` must be 'none' or all', got {tied_layers}.")
+        if isinstance(tied_layers, dict):
             # check all elements are of type list
-            for shared_layer in share_layers:
-                if not isinstance(shared_layer, list):
-                    raise ValueError(f"`share_layers` must be contain a list of list of ints, got {share_layers}.")
-                for layer in shared_layer:
-                    if not isinstance(layer, int):
-                        raise ValueError(f"`share_layers` must be contain a list of list of ints, got {share_layers}.")
-                    if layer in already_shared:
-                        raise ValueError(f"you can only share a lyaer once, got {share_layers}.")
-                    already_shared.append(layer)
+            for layer, tied_layer in tied_layers.items():
+                if not isinstance(layer, int) or not isinstance(tied_layer, int):
+                    raise ValueError(f"`tied_layers` must be a dict of ints to ints, got {tied_layers}.")
+                if layer < tied_layer:
+                    raise ValueError(f"`tied_layers` must be a dict of ints to ints, where the key is <= value got {tied_layers}.")
 
-        self.share_layers = share_layers
+        self.tied_layers = tied_layers
         super().__init__(**kwargs)
